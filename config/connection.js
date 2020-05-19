3af541e51aae4eebb77d21cd19d9d040a8d07a4d
lolobc <lauren.n.cameron@gmail.com>
2020-05-19 00:23:16 -0400

    push

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 3306 || process.env.PORT,
   user: "root",
   password: "omgwtfbbq",
   database: "burgers_db"
