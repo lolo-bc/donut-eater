d46800436458f125cdb84a886ffec46699f23c3a
lolobc <lauren.n.cameron@gmail.com>
2020-05-19 11:42:33 -0400

    push

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,29 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: "burgersonline",
-  user: "bfd81f9d05e7b9",
-  password: "7966e4a9",
-  database: "burgers_db"
-});
+var dbURL = 'mysql://bfd81f9d05e7b9:7966e4a9@us-cdbr-east-06.cleardb.net/heroku_e45707c9351e030?reconnect=true'
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306 || process.env.PORT,
+//   user: "root",
+//   password: "omgwtfbbq",
+//   database: "burgers_db"
+// });
+
+
+var connection;
+if (process.env.dbURL) {
+    connection = mysql.createConnection(process.env.dbURL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'omgwtfbbq',
+        database: 'burgers_db'
+    })
+};
 
 // Make connection.
 connection.connect(function(err) {
